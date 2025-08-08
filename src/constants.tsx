2ed199f88828ef4760920ff9de6595ce007442e9
aniketnotcoder <aniket.notcoder@gmail.com>
2025-08-08 16:16:35 +0000

    Add new features

---
+++ src/constants.tsx
@@ -0,0 +1,67 @@
+//BRAND
+export const appName = 'courseben';
+export const companyName = 'Courseben Technologies';
+export const websiteURL = 'https://courseben.com';
+export const serverURL = https://courseben.com';
+export const appLogo = 'https://firebasestorage.googleapis.com/v0/b/aicourse-81b42.appspot.com/o/aicouse.png?alt=media&token=7175cdbe-64b4-4fe4-bb6d-b519347ad8af';
+export const razorpayEnabled = true;
+export const paypalEnabled = true;
+export const stripeEnabled = false;
+export const paystackEnabled = false;
+export const flutterwaveEnabled = false;
+
+//PRICING :-
+
+//FREE 
+export const FreeType = 'Free Plan';
+export const FreeCost = 0;
+export const FreeTime = '';
+
+//MONTHLY 
+export const MonthType = 'Monthly Plan';
+export const MonthCost = 34;
+export const MonthTime = 'month';
+
+//YEARLY 
+export const YearType = 'Yearly Plan';
+export const YearCost = 79;
+export const YearTime = 'year';
+
+//TESTIMONIAL
+export const review = "The AI Course Generator revolutionized my content creation process, providing accurate and relevant topics effortlessly. It's a time-saving powerhouse that enhances the quality and relevance of my courses. A must-have tool for educators seeking efficiency and impactful online learning experiences.";
+export const from = "Md Sain Sk";
+export const profession = 'CFO at Courseben Technologies';
+export const photoURL = 'https://firebasestorage.googleapis.com/v0/b/aicourse-81b42.appspot.com/o/aicouse.png?alt=media&token=7175cdbe-64b4-4fe4-bb6d-b519347ad8af';
+
+//PAYPAL
+export const paypalPlanIdOne = "P-9CM07240FW6519903NCJRTYA";
+export const paypalPlanIdTwo = "P-464162947B524105GNCJRUQY";
+
+//RAZORPAY
+export const razorpayKeyId = "rzp_test_6SpMIBmaEksHjc";
+export const razorpayPlanIdOne = "plan_R1ziyVqSgd7FOA";
+export const razorpayPlanIdTwo = "plan_R1zk5sJPZLGvpD";
+
+//STRIPE
+export const stripePlanIdOne = "price_1OTo7CSDXmLtVnVeaHIHxqCj";
+export const stripePlanIdTwo = "price_1OTo7eSDXmLtVnVeBbn82U5B";
+
+//PAYSTACK
+export const paystackPlanIdOne = "PLN_ouqmm8eo6i2k9k8";
+export const paystackPlanIdTwo = "PLN_1v1xqb8io9t5lis";
+export const amountInZarOne = '170';
+export const amountInZarTwo = '1871';
+
+//FLUTTERWAVE
+export const flutterwavePlanIdOne = "67960";
+export const flutterwavePlanIdTwo = "67961";
+export const flutterwavePublicKey = "FLWPUBK_TEST-6ee1faf6460ea587f510a024ac4c2b23-X";
+
+//SOCIAL SIGNIN
+export const googleClientId = "39020364705-24s1ghrmda205hkdredknjmv29q2g8er.apps.googleusercontent.com";
+export const facebookClientId = "8065327730213158";
+
+//SOCIAL MEDIA
+export const facebookSocialLink = "https://www.youtube.com/@techcodebreaker";
+export const twitterSocialLink = "https://www.youtube.com/@techcodebreaker";
+export const instagramSocialLink = "https://www.youtube.com/@techcodebreaker";
